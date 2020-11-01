3b0a9d4aa828e9a86ab678c4eaaf009aba4f26fe
An Vuong <an.vuong@gooddata.com>
2020-11-01 23:09:21 +0700

    Get all documents in :global collection

--- main.rb
+++ main.rb
@@ -3,11 +3,13 @@ require 'mongo'
 Mongo::Logger.logger       = Logger.new('mongo.log')
 Mongo::Logger.logger.level = Logger::INFO
 
-client = Mongo::Client.new('mongodb+srv://readonly:readonly@covid-19.hip2i.mongodb.net/covid19')
-db = client.database
-
-puts 'collections: '
-puts db.collections # returns a list of collection objects
-
-puts 'collectionNames: '
-puts db.collection_names # returns a list of collection names
+begin
+    client = Mongo::Client.new('mongodb+srv://readonly:readonly@covid-19.hip2i.mongodb.net/covid19')
+    client[:global].find.each do |document|
+        puts document
+    end
+    client.close
+rescue Mongo::Error::NoServerAvailable => e
+    puts 'Could not connect to server'
+    puts e
+end
